61a6065392f38232c04a727cc15676f6df16b62d
Alan <foxy.studios.br@gmail.com>
2023-04-13 11:47:44 -0300

    2 butões

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+export const HomePage = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <button>Adição</button>
+            <button>Subtração</button>
+        </div>
+    )
+}
+
+export default HomePage;
