776931b4e0795d6fbf38cfc39e3dffc77d892bce
Alan <foxy.studios.br@gmail.com>
2023-04-13 12:00:44 -0300

    Adicionado função para adição e subtração

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 export const HomePage = () => {
+    const [count, setCount] = useState(0);
+
     return (
         <div>
             <h1>Home Page</h1>
-            <button>Adição</button>
-            <button>Subtração</button>
+            <h2>{count}</h2>
+
+            <button onClick={() => {
+                setCount(count + 1)
+            }}>Adição</button>
+
+            <button onClick={() => {
+                setCount(count - 1)
+            }}>Subtração</button>
         </div>
     )
 }
